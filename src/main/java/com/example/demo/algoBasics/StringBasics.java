b6fef75fb1a4ed47a1c6b416eb59ca6a44213917
HSJ <hesuijin@qq.com>
2021-05-12 22:18:34 +0800

    新增String 基础练习

---
+++ src/main/java/com/example/demo/algoBasics/StringBasics.java
@@ -0,0 +1,111 @@
+package com.example.demo.algoBasics;
+
+import java.util.Scanner;
+
+/**
+ * @Description:
+ * @Author HeSuiJin
+ * @Date 2021/5/12
+ */
+public class StringBasics {
+
+    public static void main(String[] args) {
+
+        //输入String
+        stringScannerTest();
+
+        //对String进行遍历
+        stringForTest();
+
+        //对String进行替换
+        stringReplaceTest();
+
+        //测试substring截取功能
+        stringSubstringTest();
+
+        //测试Split分割功能
+        stringSplit();
+    }
+
+
+
+
+    /**
+     * 输入String
+     */
+    private static void stringScannerTest() {
+        //  1. 键盘录入一个字符串，用 Scanner 实现
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入:");
+        String string = sc.nextLine();
+        System.out.println("输出String:" + string);
+        System.out.println("==============================================");
+    }
+
+
+    /**
+     * 对String进行遍历
+     */
+    private static void stringForTest() {
+        String string1 = "hesuijin";
+        for (int i = 0; i < string1.length(); i++) {
+            //charAt(int index)  返回指定索引处的 char 值
+            char c = string1.charAt(i);
+            System.out.println(c);
+        }
+        System.out.println("==============================================");
+        String string2 = "HSJ";
+        //  将字符串拆分为字符数组
+        char[] chars = string2.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            System.out.println(chars[i]);
+        }
+    }
+
+    /**
+     * 对Sting 使用replace进行截取
+     */
+    private static void stringReplaceTest() {
+
+        String string = "你好啊 hesuijin，血是红色的";
+        // 2. 替换敏感词
+        String result = string.replace("血","*");
+        // 3. 输出结果
+        System.out.println(result);
+    }
+
+    /**
+     * 对Sting 使用substring进行截取
+     */
+    private static void stringSubstringTest() {
+        String stringSubstring = "12345678901";
+
+        // 1. 截取字符串前三位
+        String start = stringSubstring.substring(0, 3);
+        // 2. 截取字符串后四位
+        String end = stringSubstring.substring(7);
+        // 3. 将截取后的两个字符串，中间加上****进行拼接，输出结果
+        System.out.println(start + "****" + end);
+
+    }
+
+    /**
+     * 对Sting 使用split进行切割
+     */
+    private static void stringSplit() {
+
+        String person = "HSJ,18";
+        //  根据逗号切割字符串，得到HSJ  18
+        String[] strings = person.split(",");
+
+        for (int i = 0; i < strings.length; i++) {
+            System.out.println(strings[i]);
+        }
+
+        String personSplit = "abcHSJabchesuijinabc";
+        String[] stringsSplit = personSplit.split("abc");
+        for (int i = 0; i < stringsSplit.length; i++) {
+            System.out.println(stringsSplit[i]);
+        }
+    }
+}
